f8a45e407c79972f63a2acd56ee9e1f0593b6c5a
dougjapa92 <107623956+dougjapa92@users.noreply.github.com>
2025-08-18 06:13:49 -0300

    Update CoreSideloadActivity.java

--- pkg/android/phoenix/src/com/retroarch/browser/debug/CoreSideloadActivity.java
+++ pkg/android/phoenix/src/com/retroarch/browser/debug/CoreSideloadActivity.java
@@ -1,176 +1,110 @@
 package com.retroarch.browser.debug;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.util.Log;
-import android.widget.TextView;
 
-import com.retroarch.browser.preferences.util.UserPreferences;
-import com.retroarch.browser.retroactivity.RetroActivityFuture;
+import com.retroarch.browser.mainmenu.MainMenuActivity;
 
 import java.io.File;
-import java.io.RandomAccessFile;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Callable;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
-public class CoreSideloadActivity extends Activity
-{
-    private static final String EXTRA_CORE = "LIBRETRO";
-    private static final String EXTRA_CONTENT = "ROM";
+public class CoreSideloadActivity extends Activity {
 
-    private TextView textView;
-    private CoreSideloadWorkerTask workerThread;
+    private static final String TAG = "CoreSideloadActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        textView = new TextView(this);
-        setContentView(textView);
-
-        if (!getIntent().hasExtra(EXTRA_CORE)) {
-            textView.setText("Missing extra \"LIBRETRO\"");
-            return;
-        }
-
-        workerThread = new CoreSideloadWorkerTask(
-                this,
-                textView,
-                getIntent().getStringExtra(EXTRA_CORE),
-                getIntent().getStringExtra(EXTRA_CONTENT));
-        workerThread.execute();
+        new ExtractAndLaunchTask(this).execute();
     }
 
-    @Override
-    protected void onDestroy() {
-        if (workerThread != null) {
-            workerThread.cancel(true);
-            workerThread = null;
-        }
-        super.onDestroy();
-    }
+    private static class ExtractAndLaunchTask extends AsyncTask<Void, Void, Boolean> {
+        private final Context context;
+        private ProgressDialog dialog;
 
-    private static class CoreSideloadWorkerTask extends AsyncTask<Void, Integer, String>
-    {
-        private TextView progressTextView;
-        private String core;
-        private String content;
-        private Activity ctx;
-        private File destination;
-
-        public CoreSideloadWorkerTask(Activity ctx, TextView progressTextView, String corePath, String contentPath)
-        {
-            this.progressTextView = progressTextView;
-            this.core = corePath;
-            this.ctx = ctx;
-            this.content = contentPath;
+        ExtractAndLaunchTask(Context ctx) {
+            this.context = ctx;
         }
 
         @Override
         protected void onPreExecute() {
-            super.onPreExecute();
-            progressTextView.setText("Sideloading (multithread)...");
+            dialog = new ProgressDialog(context);
+            dialog.setMessage("Preparando RetroArch...");
+            dialog.setCancelable(false);
+            dialog.show();
         }
 
         @Override
-        protected String doInBackground(Void... voids) {
+        protected Boolean doInBackground(Void... voids) {
             try {
-                File coreFile = new File(core);
-                File corePath = new File(UserPreferences.getPreferences(ctx)
-                        .getString("libretro_path", ctx.getApplicationInfo().dataDir + "/cores/"));
+                File baseDir = new File(MainMenuActivity.RETROARCH_DIR, "assets");
+                if (!baseDir.exists() && !baseDir.mkdirs()) return false;
 
-                if (!coreFile.exists())
-                    return "Input file doesn't exist (" + core + ")";
-                if (!corePath.exists())
-                    return "Destination directory doesn't exist (" + corePath.getAbsolutePath() + ")";
+                String[] assetFiles = context.getAssets().list("android");
+                if (assetFiles == null) return false;
 
-                destination = new File(corePath, coreFile.getName());
+                ExecutorService executor = Executors.newFixedThreadPool(
+                        Math.max(2, Runtime.getRuntime().availableProcessors())
+                );
 
-                long fileSize = coreFile.length();
-                int threads = Runtime.getRuntime().availableProcessors();
-                long chunkSize = fileSize / threads;
-
-                ExecutorService executor = Executors.newFixedThreadPool(threads);
-                List<Future<Boolean>> futures = new ArrayList<>();
-
-                for (int i = 0; i < threads; i++) {
-                    final int index = i;
-                    futures.add(executor.submit(new Callable<Boolean>() {
-                        @Override
-                        public Boolean call() {
-                            try (RandomAccessFile in = new RandomAccessFile(coreFile, "r");
-                                 RandomAccessFile out = new RandomAccessFile(destination, "rw")) {
-
-                                long start = index * chunkSize;
-                                long end = (index == threads - 1) ? fileSize : (start + chunkSize);
-
-                                byte[] buffer = new byte[8192];
-                                in.seek(start);
-                                out.seek(start);
-
-                                long pos = start;
-                                while (pos < end) {
-                                    int toRead = (int) Math.min(buffer.length, end - pos);
-                                    int read = in.read(buffer, 0, toRead);
-                                    if (read == -1) break;
-                                    out.write(buffer, 0, read);
-                                    pos += read;
-                                }
-                                return true;
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                                return false;
-                            }
-                        }
-                    }));
-                }
-
-                // Aguarda todas as threads
-                for (Future<Boolean> f : futures) {
-                    if (!f.get()) {
-                        executor.shutdown();
-                        return "Erro em thread de cópia";
-                    }
+                for (String name : assetFiles) {
+                    executor.submit(() -> copyAsset(context, "android/" + name,
+                            new File(baseDir, name)));
                 }
 
                 executor.shutdown();
+                while (!executor.isTerminated()) {
+                    Thread.sleep(100);
+                }
 
-                return null; // sucesso
+                // atualizar retroarch.cfg
+                File cfgFile = new File(MainMenuActivity.RETROARCH_DIR, "retroarch.cfg");
+                MainMenuActivity.ExtractAssetsTask.updateRetroarchCfg(cfgFile);
+
+                return true;
             } catch (Exception e) {
-                e.printStackTrace();
-                return e.getMessage();
+                Log.e(TAG, "Erro preparando RetroArch", e);
+                return false;
             }
         }
 
         @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
+        protected void onPostExecute(Boolean ok) {
+            if (dialog != null && dialog.isShowing()) dialog.dismiss();
 
-            if (s == null) {
-                progressTextView.setText("Done!");
+            if (ok) {
+                // Aqui chamamos a RetroActivity diretamente
+                try {
+                    Intent launchIntent = new Intent();
+                    launchIntent.setClassName(context, "com.retroarch.browser.retroactivity.RetroActivityFuture");
+                    context.startActivity(launchIntent);
+                } catch (Exception e) {
+                    Log.e(TAG, "Falha ao iniciar RetroArch", e);
+                }
+            }
 
-                Intent retro = new Intent(ctx, RetroActivityFuture.class);
-                retro.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                retro.putExtra("ROM", content);
-                retro.putExtra("LIBRETRO", destination.getAbsolutePath());
-                retro.putExtra("CONFIGFILE", UserPreferences.getDefaultConfigPath(ctx));
-                retro.putExtra("IME", Settings.Secure.getString(ctx.getContentResolver(), Settings.Secure.DEFAULT_INPUT_METHOD));
-                retro.putExtra("DATADIR", ctx.getApplicationInfo().dataDir);
-                retro.putExtra("APKPATH", ctx.getApplicationInfo().sourceDir);
+            if (context instanceof Activity) ((Activity) context).finish();
+        }
 
-                Log.d("sideload", "Running RetroArch with core " + destination.getAbsolutePath());
-
-                ctx.startActivity(retro);
-                ctx.finish();
-            } else {
-                progressTextView.setText("Error: " + s);
+        private static void copyAsset(Context ctx, String assetName, File outFile) {
+            try (InputStream is = ctx.getAssets().open(assetName);
+                 OutputStream os = new FileOutputStream(outFile)) {
+                byte[] buffer = new byte[8192];
+                int read;
+                while ((read = is.read(buffer)) > 0) {
+                    os.write(buffer, 0, read);
+                }
+            } catch (Exception e) {
+                Log.e(TAG, "Erro copiando " + assetName, e);
             }
         }
     }

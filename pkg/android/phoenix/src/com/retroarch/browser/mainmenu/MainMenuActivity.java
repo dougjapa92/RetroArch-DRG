00187c49e0b506e061c7a808b2943fe9ad182ee6
dougjapa92 <107623956+dougjapa92@users.noreply.github.com>
2025-08-18 06:44:51 -0300

    Update MainMenuActivity.java

--- pkg/android/phoenix/src/com/retroarch/browser/mainmenu/MainMenuActivity.java
+++ pkg/android/phoenix/src/com/retroarch/browser/mainmenu/MainMenuActivity.java
@@ -1,119 +1,192 @@
 package com.retroarch.browser.mainmenu;
 
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.os.AsyncTask;
+import com.retroarch.browser.preferences.util.UserPreferences;
+import com.retroarch.browser.retroactivity.RetroActivityFuture;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
+
+import java.util.List;
+import java.util.ArrayList;
+import android.content.pm.PackageManager;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.app.AlertDialog;
 import android.util.Log;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+/**
+ * {@link PreferenceActivity} subclass that provides all of the
+ * functionality of the main menu screen.
+ */
+public final class MainMenuActivity extends PreferenceActivity
+{
+	final private int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 124;
+	public static String PACKAGE_NAME;
+	boolean checkPermissions = false;
 
-public class MainMenuActivity extends Activity {
+	public void showMessageOKCancel(String message, DialogInterface.OnClickListener onClickListener)
+	{
+		new AlertDialog.Builder(this).setMessage(message)
+			.setPositiveButton("OK", onClickListener).setCancelable(false)
+			.setNegativeButton("Cancel", null).create().show();
+	}
 
-    private static final String TAG = "MainMenuActivity";
-    public static final String RETROARCH_DIR =
-            "/storage/emulated/0/Android/media/com.retroarch/";
+	private boolean addPermission(List<String> permissionsList, String permission)
+	{
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M)
+		{
+			if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED)
+			{
+				permissionsList.add(permission);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        new ExtractAssetsTask(this).execute();
-    }
+				// Check for Rationale Option
+				if (!shouldShowRequestPermissionRationale(permission))
+					return false;
+			}
+		}
 
-    public static class ExtractAssetsTask extends AsyncTask<Void, Void, Boolean> {
-        private final Context context;
-        private ProgressDialog dialog;
+		return true;
+	}
 
-        ExtractAssetsTask(Context ctx) {
-            this.context = ctx;
-        }
+	public void checkRuntimePermissions()
+	{
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M)
+		{
+			// Android 6.0+ needs runtime permission checks
+			List<String> permissionsNeeded = new ArrayList<String>();
+			final List<String> permissionsList = new ArrayList<String>();
 
-        @Override
-        protected void onPreExecute() {
-            dialog = new ProgressDialog(context);
-            dialog.setMessage("Extraindo arquivos, aguarde...");
-            dialog.setCancelable(false);
-            dialog.show();
-        }
+			if (!addPermission(permissionsList, Manifest.permission.READ_EXTERNAL_STORAGE))
+				permissionsNeeded.add("Read External Storage");
+			if (!addPermission(permissionsList, Manifest.permission.WRITE_EXTERNAL_STORAGE))
+				permissionsNeeded.add("Write External Storage");
 
-        @Override
-        protected Boolean doInBackground(Void... voids) {
-            try {
-                File baseDir = new File(RETROARCH_DIR, "assets");
-                if (!baseDir.exists() && !baseDir.mkdirs()) {
-                    Log.e(TAG, "Falha ao criar " + baseDir.getAbsolutePath());
-                    return false;
-                }
+			if (permissionsList.size() > 0)
+			{
+				checkPermissions = true;
 
-                String[] assetFiles = context.getAssets().list("android");
-                if (assetFiles == null) return false;
+				if (permissionsNeeded.size() > 0)
+				{
+					// Need Rationale
+					Log.i("MainMenuActivity", "Need to request external storage permissions.");
 
-                ExecutorService executor = Executors.newFixedThreadPool(
-                        Math.max(2, Runtime.getRuntime().availableProcessors())
-                );
+					String message = "You need to grant access to " + permissionsNeeded.get(0);
 
-                for (String name : assetFiles) {
-                    executor.submit(() -> copyAsset(context, "android/" + name,
-                            new File(baseDir, name)));
-                }
+					for (int i = 1; i < permissionsNeeded.size(); i++)
+						message = message + ", " + permissionsNeeded.get(i);
 
-                executor.shutdown();
-                while (!executor.isTerminated()) {
-                    Thread.sleep(100);
-                }
+					showMessageOKCancel(message,
+						new DialogInterface.OnClickListener()
+						{
+							@Override
+							public void onClick(DialogInterface dialog, int which)
+							{
+								if (which == AlertDialog.BUTTON_POSITIVE)
+								{
+									requestPermissions(permissionsList.toArray(new String[permissionsList.size()]),
+											REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
 
-                // Atualizar retroarch.cfg
-                File cfgFile = new File(RETROARCH_DIR, "retroarch.cfg");
-                updateRetroarchCfg(cfgFile);
+									Log.i("MainMenuActivity", "User accepted request for external storage permissions.");
+								}
+							}
+						});
+				}
+				else
+				{
+					requestPermissions(permissionsList.toArray(new String[permissionsList.size()]),
+						REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
 
-                return true;
-            } catch (Exception e) {
-                Log.e(TAG, "Erro na extração", e);
-                return false;
-            }
-        }
+					Log.i("MainMenuActivity", "Requested external storage permissions.");
+				}
+			}
+		}
 
-        @Override
-        protected void onPostExecute(Boolean ok) {
-            if (dialog != null && dialog.isShowing()) dialog.dismiss();
-            if (context instanceof Activity) ((Activity) context).finish();
-        }
+		if (!checkPermissions)
+		{
+			finalStartup();
+		}
+	}
 
-        private static void copyAsset(Context ctx, String assetName, File outFile) {
-            try (InputStream is = ctx.getAssets().open(assetName);
-                 OutputStream os = new FileOutputStream(outFile)) {
-                byte[] buffer = new byte[8192];
-                int read;
-                while ((read = is.read(buffer)) > 0) {
-                    os.write(buffer, 0, read);
-                }
-            } catch (Exception e) {
-                Log.e(TAG, "Erro copiando " + assetName, e);
-            }
-        }
+	public void finalStartup()
+	{
+		final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		Intent retro = new Intent(this, RetroActivityFuture.class);
 
-        public static void updateRetroarchCfg(File cfgFile) {
-            try (FileOutputStream fos = new FileOutputStream(cfgFile, false)) {
-                String cfg =
-                        "audio_out_rate = \"48000\"\n" +
-                        "audio_block_frames = \"192\"\n" +
-                        "bundle_assets_dst_path = \"" + RETROARCH_DIR + "assets\"\n" +
-                        "libretro_directory = \"" + RETROARCH_DIR + "cores\"\n" +
-                        "system_directory = \"" + RETROARCH_DIR + "system\"\n" +
-                        "screenshot_directory = \"" + RETROARCH_DIR + "screenshots\"\n" +
-                        "savefile_directory = \"" + RETROARCH_DIR + "saves\"\n" +
-                        "savestate_directory = \"" + RETROARCH_DIR + "states\"\n" +
-                        "playlist_directory = \"" + RETROARCH_DIR + "playlists\"\n";
-                fos.write(cfg.getBytes());
-            } catch (Exception e) {
-                Log.e(TAG, "Erro atualizando retroarch.cfg", e);
-            }
-        }
-    }
-} 
+		retro.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+		startRetroActivity(
+				retro,
+				null,
+				prefs.getString("libretro_path", getApplicationInfo().dataDir + "/cores/"),
+				UserPreferences.getDefaultConfigPath(this),
+				Settings.Secure.getString(getContentResolver(), Settings.Secure.DEFAULT_INPUT_METHOD),
+				getApplicationInfo().dataDir,
+				getApplicationInfo().sourceDir);
+		startActivity(retro);
+		finish();
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+	{
+		switch (requestCode)
+		{
+			case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS:
+				for (int i = 0; i < permissions.length; i++)
+				{
+					if(grantResults[i] == PackageManager.PERMISSION_GRANTED)
+					{
+						Log.i("MainMenuActivity", "Permission: " + permissions[i] + " was granted.");
+					}
+					else
+					{
+						Log.i("MainMenuActivity", "Permission: " + permissions[i] + " was not granted.");
+					}
+				}
+
+				break;
+			default:
+				super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+				break;
+		}
+
+		finalStartup();
+	}
+
+	public static void startRetroActivity(Intent retro, String contentPath, String corePath,
+			String configFilePath, String imePath, String dataDirPath, String dataSourcePath)
+	{
+		if (contentPath != null) {
+			retro.putExtra("ROM", contentPath);
+		}
+		retro.putExtra("LIBRETRO", corePath);
+		retro.putExtra("CONFIGFILE", configFilePath);
+		retro.putExtra("IME", imePath);
+		retro.putExtra("DATADIR", dataDirPath);
+		retro.putExtra("APK", dataSourcePath);
+		retro.putExtra("SDCARD", Environment.getExternalStorageDirectory().getAbsolutePath());
+		String external = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Android/data/" + PACKAGE_NAME + "/files";
+		retro.putExtra("EXTERNAL", external);
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		PACKAGE_NAME = getPackageName();
+
+		// Bind audio stream to hardware controls.
+		setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+		UserPreferences.updateConfigFile(this);
+
+		checkRuntimePermissions();
+	}
+}

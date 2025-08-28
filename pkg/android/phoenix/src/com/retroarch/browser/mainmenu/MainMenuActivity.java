6ac15f6399e763af848291683baef385285559b8
dougjapa92 <107623956+dougjapa92@users.noreply.github.com>
2025-08-28 09:49:03 -0300

    Populando CFG 4 MainMenuActivity.java

--- pkg/android/phoenix/src/com/retroarch/browser/mainmenu/MainMenuActivity.java
+++ pkg/android/phoenix/src/com/retroarch/browser/mainmenu/MainMenuActivity.java
@@ -33,6 +33,8 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;

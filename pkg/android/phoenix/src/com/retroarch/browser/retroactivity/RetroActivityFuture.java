9774f4e861c9048b493490735148e22d62af1c1b
dougjapa92 <107623956+dougjapa92@users.noreply.github.com>
2025-09-03 00:58:29 -0300

    Update RetroActivityFuture.java

--- pkg/android/phoenix/src/com/retroarch/browser/retroactivity/RetroActivityFuture.java
+++ pkg/android/phoenix/src/com/retroarch/browser/retroactivity/RetroActivityFuture.java
@@ -168,7 +168,7 @@ public final class RetroActivityFuture extends RetroActivityCamera {
                 handler.post(countdownRunnable);
             });
     
-            dialog.show();
+            handler.postDelayed(() -> dialog.show(), 50);
         });
     
         try {

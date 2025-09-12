3f302cad23f57910417e30938a75661e742e0a25
dougjapa92 <107623956+dougjapa92@users.noreply.github.com>
2025-09-12 17:34:29 -0300

    Update MainMenuActivity.java

--- pkg/android/phoenix/src/com/retroarch/browser/mainmenu/MainMenuActivity.java
+++ pkg/android/phoenix/src/com/retroarch/browser/mainmenu/MainMenuActivity.java
@@ -223,10 +223,11 @@ public final class MainMenuActivity extends PreferenceActivity {
     
         // 2. Criar e configurar o Título
         TextView titleView = new TextView(context);
-        titleView.setText("Escolha a proporção de tela dos jogos:");
-        titleView.setTextSize(16); // Tamanho da fonte em SP
+        titleView.setText("Escolha a proporção de tela dos jogos: ");
+        titleView.setTextSize(18); // Tamanho da fonte em SP
         titleView.setGravity(Gravity.CENTER);
         titleView.setTextColor(Color.BLACK);
+        titleView.setTypeface(titleView.getTypeface(), Typeface.BOLD);
     
         // Adicionar um espaçamento abaixo do título
         LinearLayout.LayoutParams titleParams = new LinearLayout.LayoutParams(
